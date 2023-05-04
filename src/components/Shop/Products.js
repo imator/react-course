c8e5da0a0825f755cc357e4d217a99f26d37b895
Ahmet ORMAN <a.orman37@gmail.com>
2023-05-04 19:53:39 +0300

    03 Mayıs 2023 tarihli çalışmalar yüklendi

---
+++ src/components/Shop/Products.js
@@ -0,0 +1,29 @@
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  { id: "p1", price: 6, title: "My First Book", description: "This is a first product - amazing!" },
+  { id: "p2", price: 12, title: "Cool Caffe", description: "This is the best Caffe in the World" },
+  { id: "p3", price: 352, title: "Computer", description: "My developer Computer" },
+];
+
+const Products = (props) => {
+  return (
+    <section className={classes.products}>
+      <h2>Buy your favorite products</h2>
+      <ul>
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
